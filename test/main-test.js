24a4d891c02129b8d63116ca27accb1940d98bff
rsbondi <r_bondi@hotmail.com>
2017-12-06 14:41:44 -0600

    test script

--- test/main-test.js
+++ test/main-test.js
@@ -13,11 +13,21 @@ async function runTests() {
   try {
     await app.start()
     const isVisible = await app.browserWindow.isVisible()
-    assert.equal(isVisible, false, 'Window is visible')
+    assert.equal(isVisible, true, 'Window is visible')
   
     const title = await app.client.getTitle()
     assert.equal(title, 'bitcoin RPC console', 'Verify Title')
-  
+
+    const logs = await app.client.getRenderProcessLogs();
+    // Print renderer process logs
+    const errors = logs.filter(log => {
+      console.log(log.message);
+      console.log(log.source);
+      console.log(log.level);
+      return log.level == "ERROR"
+    });
+    assert.equal(errors.length, 0, 'no render logs'); // deprecation warning, how can I fix???
+    
     console.log('All tests passed')
     app.stop()
 
