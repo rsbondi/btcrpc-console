8d4feb6cb385567f1833f9e38961445afcff0f18
Richard Bondi <r_bondi@hotmail.com>
2017-12-07 11:35:15 -0600

    insert parse by token instead of word

--- src/js/resultEditor.js
+++ src/js/resultEditor.js
@@ -35,13 +35,22 @@ class ResultEditor {
       contextMenuGroupId: 'navigation',
       contextMenuOrder: 1.1,
       run: function (ed) {
-        const word = ed.getModel().getWordAtPosition(ed.getPosition())
+        const line = ed.getModel().getLineContent(ed.getPosition().lineNumber)
+        const tokens = monaco.editor.tokenize(line, 'javascript')[0]
+        let t = tokens.length - 1
+        for(; ~t; t--) {
+            const token = tokens[t]
+            if(token.offset <= ed.getPosition().column)
+            break
+        }
+        const token = tokens[t]
+        const word = line.slice(token.offset, t==tokens.length-1 ? line.length : tokens[t+1].offset)
         if (word) {
           const cmd = self.commandEditor.getPosition()
           self.commandEditor.executeEdits('', [
-            { range: commandEditor.getSelection().cloneRange(), text: word.word }
+            { range: commandEditor.getSelection().cloneRange(), text: word }
           ])
-          const col = cmd.column + word.word.length
+          const col = cmd.column + word.length
           self.commandEditor.setSelection(new monaco.Range(cmd.lineNumber, col, cmd.lineNumber, col))
           self.commandEditor.focus()
         }
